6f60e131b3f3cf08db6b31a056f6160513bcb3a6
Seba <100377036+dev-Seba@users.noreply.github.com>
2022-10-03 18:03:21 +0200

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Test {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Podaj liczbe, ja dodam 1 ");
-        Integer c = scan.nextInt();
-        System.out.println(c+1);
-    }
-}
